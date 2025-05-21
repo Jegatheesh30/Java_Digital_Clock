ec2070cabdd839a09ce7164b1ef650b0e0884721
Jegatheesh30 <jegatheesh3096@gmail.com>
2025-05-21 21:35:38 +0530

    clock project first commit

--- JavaDigitalClock/src/javadigitalclock/DigitalClock.java
+++ JavaDigitalClock/src/javadigitalclock/DigitalClock.java
@@ -48,7 +48,7 @@ public class DigitalClock extends javax.swing.JFrame {
             }
         }.start();
     }
-
+    //jegathees put this comment
     /**
      * This method is called from within the constructor to initialize the form.
      * WARNING: Do NOT modify this code. The content of this method is always
